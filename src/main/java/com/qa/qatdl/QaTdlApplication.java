1d0c7a7290f43c66b1cabd76533821ef2989c534
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-02-10 15:32:59 +0000

    initial commit

---
+++ src/main/java/com/qa/qatdl/QaTdlApplication.java
@@ -0,0 +1,13 @@
+package com.qa.qatdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaTdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaTdlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qa/qatdl/QaTdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.qatdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaTdlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
