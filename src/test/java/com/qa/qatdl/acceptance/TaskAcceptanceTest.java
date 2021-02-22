a00142582d4b4883327e13a47224965a36334bd7
henry-oliver-edwards <HOliver-Edwards@academytrainee.com>
2021-02-22 16:54:39 +0000

    Acceptance tests added

---
+++ src/test/java/com/qa/qatdl/acceptance/TaskAcceptanceTest.java
@@ -0,0 +1,4 @@
+package com.qa.qatdl.acceptance;
+
+public class TaskAcceptanceTest {
+}
--- src/test/java/com/qa/qatdl/acceptance/ToDoListAcceptanceTest.java
+++ src/test/java/com/qa/qatdl/acceptance/ToDoListAcceptanceTest.java
@@ -1,5 +1,6 @@
 package com.qa.qatdl.acceptance;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
@@ -9,38 +10,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@Disabled
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Sql(scripts = "classpath:test-drop-all.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@Sql(scripts = {"classpath:test-schema.sql", "classpath:test-data.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@ActiveProfiles("dev")
 public class ToDoListAcceptanceTest {
-
-    @TestConfiguration
-    static class AppConfig {
-        @Bean
-        public WebDriver webDriver() {
-            return new FirefoxDriver();
-        }
-    }
 
     @Autowired
     private static WebDriver driver;
 
     private static WebElement element;
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-
 
     @BeforeAll
     public static void setup() {
@@ -57,15 +57,66 @@ public class ToDoListAcceptanceTest {
 
     @Test
     public void createToDoListTest() throws InterruptedException {
-        driver.navigate().to("http://localhost:" + port + "/index.html");
+        Thread.sleep(500);
+        driver.get("http://localhost:8393");
+
+        element = driver.findElement(By.cssSelector("body > main > div > div > div.col-3 > div > div:nth-child(1) > a"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-menu > a:nth-child(1)"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
 
         element = driver.findElement(By.cssSelector("#to-do-list-name"));
         element.sendKeys("Shopping List");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
 
         element = driver.findElement(By.cssSelector("#to-do-list-create > form:nth-child(2) > button:nth-child(3)"));
         element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+        Thread.sleep(1000);
 
-        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+        element = driver.findElement(By.cssSelector("#table-body > tr:nth-child(1) > th:nth-child(1)"));
+        assertEquals(element.getText(), "1");
+
+        element = driver.findElement(By.cssSelector("#table-body > tr:nth-child(1) > td:nth-child(2)"));
+        assertEquals(element.getText(), "3");
+
+        element = driver.findElement(By.cssSelector("#table-body > tr:nth-child(1) > td:nth-child(3)"));
+        assertEquals(element.getText(), "Shopping List");
+
+    }
+
+    @Test
+    public void readToDoListTest() throws InterruptedException {
+        Thread.sleep(500);
+        driver.get("http://localhost:8393");
+
+        element = driver.findElement(By.cssSelector("body > main > div > div > div.col-3 > div > div:nth-child(1) > a"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-menu > a:nth-child(2)"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-read-all"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+        Thread.sleep(1000);
+
+        String elementText = driver.findElements(By.cssSelector("#table-body")).stream().map(WebElement::getText).collect(Collectors.toList()).toString().replace("\n", " ");
+        System.out.println(elementText);
+        String expected = "[1 1 General Chicken, Ham 2 2 Music To Buy]";
+        assertEquals(expected, elementText);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-read-id"));
+        element.sendKeys("1");
+
+        element = driver.findElement(By.cssSelector("#to-do-list-read > form > div > div:nth-child(3) > button"));
+        element.click();
+        Thread.sleep(1000);
 
         element = driver.findElement(By.cssSelector("#table-body > tr:nth-child(1) > th:nth-child(1)"));
         assertEquals(element.getText(), "1");
@@ -74,7 +125,64 @@ public class ToDoListAcceptanceTest {
         assertEquals(element.getText(), "1");
 
         element = driver.findElement(By.cssSelector("#table-body > tr:nth-child(1) > td:nth-child(3)"));
-        assertEquals(element.getText(), "Shopping List");
+        assertEquals(element.getText(), "General");
+    }
 
+    @Test
+    public void updateToDoListTest() throws InterruptedException {
+        Thread.sleep(500);
+        driver.get("http://localhost:8393");
+
+        element = driver.findElement(By.cssSelector("body > main > div > div > div.col-3 > div > div:nth-child(1) > a"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-menu > a:nth-child(3)"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-update-id"));
+        element.sendKeys("1");
+
+        element = driver.findElement(By.cssSelector("#to-do-list-new-name"));
+        element.sendKeys("Games To Get");
+
+        element = driver.findElement(By.cssSelector("#to-do-list-update > form > div:nth-child(2) > button"));
+        element.click();
+
+        element = driver.findElement(By.cssSelector("#table-body > tr:nth-child(1) > th:nth-child(1)"));
+        assertEquals(element.getText(), "1");
+
+        element = driver.findElement(By.cssSelector("#table-body > tr:nth-child(1) > td:nth-child(2)"));
+        assertEquals(element.getText(), "1");
+
+        element = driver.findElement(By.cssSelector("#table-body > tr:nth-child(1) > td:nth-child(3)"));
+        assertEquals(element.getText(), "Games To Get");
+    }
+
+    @Test
+    public void deleteToDoListTest() throws InterruptedException {
+        Thread.sleep(500);
+        driver.get("http://localhost:8393");
+
+        element = driver.findElement(By.cssSelector("body > main > div > div > div.col-3 > div > div:nth-child(1) > a"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-menu > a:nth-child(4)"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-delete-id"));
+        element.sendKeys("1");
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+
+        element = driver.findElement(By.cssSelector("#to-do-list-delete > form > button"));
+        element.click();
+        driver.manage().timeouts().implicitlyWait(1000, TimeUnit.MILLISECONDS);
+        Thread.sleep(1000);
+
+        element = driver.findElement(By.cssSelector("#table-body"));
+        assertEquals("", element.getText());
     }
 }
